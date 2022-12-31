f58e38d60cc8d8432cb3732da0702d42347fcc7e
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-12-31 14:10:04 +0530

    tags, url

--- template_ex_proj/my_app/urls.py
+++ template_ex_proj/my_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# This registers the app namespace
+app_name = 'my_app'
+
 urlpatterns = [
     path('', views.example_view, name='example_view_path'),
     path('variables', views.variable_view, name='variable_view_path')
