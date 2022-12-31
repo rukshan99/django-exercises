14efce7042d36b24d33f6607376507c5ef2d0aaf
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-12-31 13:41:28 +0530

    tags, if else

--- template_ex_proj/my_app/views.py
+++ template_ex_proj/my_app/views.py
@@ -9,6 +9,7 @@ def variable_view(request):
         'first_name': 'RuksHan',
         'last_name': 'Jayasekara',
         'some_list': [1,2,3],
-        'some_dict': {'some_key': 'some_value'}
+        'some_dict': {'some_key': 'some_value'},
+        'user_logged_in': False
     }
     return render(request, 'my_app/variable.html', context=my_var)
