46e627529cdfb527ce6c84a420a9d189be8e4287
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-12-31 16:13:04 +0530

    handling static files

--- template_ex_proj/template_ex_proj/settings.py
+++ template_ex_proj/template_ex_proj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$gx&we+^%d2qmu(766@ro2qt%diwdisc7yyep^v%(q$)ogzj7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
