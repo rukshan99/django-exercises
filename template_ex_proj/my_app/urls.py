314c642e12ccfbe5dba8a5b1c6b734d6b5d497b5
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-12-31 11:38:07 +0530

    context, variables and filters in django templates

--- template_ex_proj/my_app/urls.py
+++ template_ex_proj/my_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.example_view, name='example_view_path')
+    path('', views.example_view, name='example_view_path'),
+    path('variables', views.variable_view, name='variable_view_path')
 ]
--- template_ex_proj/my_app/views.py
+++ template_ex_proj/my_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+    my_var = {
+        'first_name': 'RuksHan',
+        'last_name': 'Jayasekara',
+        'some_list': [1,2,3],
+        'some_dict': {'some_key': 'some_value'}
+    }
+    return render(request, 'my_app/variable.html', context=my_var)
