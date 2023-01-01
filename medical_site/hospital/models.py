3c53980cb5be6080e78da5aab82d5cf8d4d827eb
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-01 18:04:09 +0530

    update models,tables

--- medical_site/hospital/models.py
+++ medical_site/hospital/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(150)])
+    heart_rate = models.IntegerField(default=75, validators=[MinValueValidator(15), MaxValueValidator(600)])
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 #python manage.py sqlmigrate hospital 0001 --> to see the created sql queries
